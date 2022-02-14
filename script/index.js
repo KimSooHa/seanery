1e1db77e519099dfaba349745f8e7b1efb6c13a4
soohakim <tngk1001@naver.com>
2022-02-15 03:23:39 +0900

    buttons.css 빼기/더하기 버튼 opacity 추가 / index 페이지 인원수 선택 자바스크립트 추가 / select-opt-num 페이지 동적구현

--- script/index.js
+++ script/index.js
@@ -32,3 +32,86 @@ flatpickr("#end-date", {
 // });
 
 
+window.addEventListener("load", function() {
+ 
+  var section = document.querySelector(".simple-search");
+  var selectNum = section.querySelector(".select-num");
+  var adult = selectNum.querySelector(".adult");
+  var kid = selectNum.querySelector(".kid");
+  var adultInput = adult.querySelector("input");
+  var kidInput = kid.querySelector("input");
+  var adultPlus = adult.querySelector(".img-btn-plus");
+  var adultMinus = adult.querySelector(".img-btn-minus");
+  var kidPlus = kid.querySelector(".img-btn-plus");
+  var kidMinus = kid.querySelector(".img-btn-minus");
+  // var minus = box.querySelector(".img-btn-minus");
+  
+
+
+  selectNum.addEventListener("click", function(e) {
+
+
+    if(e.target.nodeName != "SPAN")
+        return;
+
+    // console.log("clicked");
+    var adultValue = adultInput.value;
+    var adultInt = parseInt(adultValue);
+
+    
+    if(e.target == adultPlus) {
+      
+      if(adultInt >= 1)
+        return;
+        
+      adultInput.value = adultInt + 1;
+    } 
+    else if(e.target == adultMinus) {
+      if(adultInt <= 0)
+        return;
+
+      adultInput.value = adultInt - 1;
+
+    }
+
+    var kidValue = kidInput.value;
+    var kidInt = parseInt(kidValue);
+
+    if(e.target == kidPlus) {
+      
+      if(kidInt >= 1)
+        return;
+        
+      kidInput.value = kidInt + 1;
+    } 
+    else if(e.target == kidMinus) {
+      if(kidInt <= 0)
+        return;
+
+      kidInput.value = kidInt - 1;
+
+    }
+
+    
+
+  });
+
+  selectNum.addEventListener("mousedown", function(e) {
+
+    if(e.target.nodeName != "SPAN")
+        return;
+
+    e.target.style.opacity = "0.7";
+    
+  });
+
+  selectNum.addEventListener("mouseup", function(e) {
+
+    if(e.target.nodeName != "SPAN")
+        return;
+
+    e.target.style.opacity = "1";
+  });
+
+
+});
