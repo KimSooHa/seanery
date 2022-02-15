d2b959c718444fd8e6df8df0f5aae1bd4196d79b
soohakim <tngk1001@naver.com>
2022-02-15 21:24:06 +0900

    index.js 인원수 선택 성인 최대값 수정, 주석 추가

--- script/index.js
+++ script/index.js
@@ -32,6 +32,9 @@ flatpickr("#end-date", {
 // });
 
 
+// --- simple-search --------------------------------------
+
+// select-num
 window.addEventListener("load", function() {
  
   var section = document.querySelector(".simple-search");
@@ -55,28 +58,33 @@ window.addEventListener("load", function() {
         return;
 
     // console.log("clicked");
+
+    // 성인
     var adultValue = adultInput.value;
     var adultInt = parseInt(adultValue);
 
-    
+    // 더하기 버튼
     if(e.target == adultPlus) {
       
-      if(adultInt >= 1)
+      if(adultInt >= 2)
         return;
         
       adultInput.value = adultInt + 1;
     } 
+    // 빼기 버튼
     else if(e.target == adultMinus) {
-      if(adultInt <= 0)
+      if(adultInt <= 1)
         return;
 
       adultInput.value = adultInt - 1;
 
     }
 
+    // 아동
     var kidValue = kidInput.value;
     var kidInt = parseInt(kidValue);
 
+    // 더하기 버튼
     if(e.target == kidPlus) {
       
       if(kidInt >= 1)
@@ -84,6 +92,7 @@ window.addEventListener("load", function() {
         
       kidInput.value = kidInt + 1;
     } 
+    // 빼기 버튼
     else if(e.target == kidMinus) {
       if(kidInt <= 0)
         return;
@@ -96,6 +105,7 @@ window.addEventListener("load", function() {
 
   });
 
+  // 마우스 눌릴때
   selectNum.addEventListener("mousedown", function(e) {
 
     if(e.target.nodeName != "SPAN")
@@ -105,6 +115,7 @@ window.addEventListener("load", function() {
     
   });
 
+  // 마우스 올라갈 때
   selectNum.addEventListener("mouseup", function(e) {
 
     if(e.target.nodeName != "SPAN")
