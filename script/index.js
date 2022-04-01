934e5e036b3b543cfee710aa5f359098c21c3377
soohakim <tngk1001@naver.com>
2022-04-02 06:03:52 +0900

    index.js 오류부분 주석처리 / user/menu/list html 및 css 스타일 수정

--- script/index.js
+++ script/index.js
@@ -24,30 +24,30 @@ window.addEventListener("load", function(){
 
 // // commonjs
 // const flatpickr = require("flatpickr");
-const flapickr = document.querySelectorAll(".flatpickr");
-// const startDate = document.querySelector(".start-date");
-// const endDate = document.querySelector(".end-date");
+// const flapickr = document.querySelectorAll(".flatpickr");
+// // const startDate = document.querySelector(".start-date");
+// // const endDate = document.querySelector(".end-date");
 
-// flatpickr(".datepicker");
-// Flatpickr.setDefaults({});
-// flatpickr("#flatpickr-demo", {})
-// import rangePlugin from "plugins/rangePlugin";
+// // flatpickr(".datepicker");
+// // Flatpickr.setDefaults({});
+// // flatpickr("#flatpickr-demo", {})
+// // import rangePlugin from "plugins/rangePlugin";
 
-flatpickr("#start-date", {
-  mode: "range",
-  minDate: "today",
-  allowInput: true,
-  dateFormat: "Y-m-d",
-  // "plugins": [new rangePlugin({ input: "#end-date"})]
-});
+// flatpickr("#start-date", {
+//   mode: "range",
+//   minDate: "today",
+//   allowInput: true,
+//   dateFormat: "Y-m-d",
+//   // "plugins": [new rangePlugin({ input: "#end-date"})]
+// });
   
   
-flatpickr("#end-date", {
-  mode: "range",
-  minDate: "today",
-  allowInput: true,
-  dateFormat: "Y-m-d",
-});
+// flatpickr("#end-date", {
+//   mode: "range",
+//   minDate: "today",
+//   allowInput: true,
+//   dateFormat: "Y-m-d",
+// });
 
 // rangePlugin 호환문제 해결 못함...
 // 참고: https://flatpickr.js.org/plugins/
