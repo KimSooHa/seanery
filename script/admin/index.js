66c7616d3381525bbca97a9fc9ad2cae943be87f
choyi918 <choyi918@naver.com>
2022-05-06 02:28:00 +0900

    dash-board 섹션의 nav bar 버튼 눌렀을 때 스타일 바꾸기

--- script/admin/index.js
+++ script/admin/index.js
@@ -1,3 +1,28 @@
 window.addEventListener("load", function(){
+    const dashBoardsection = document.querySelector(".dash-board");
+    const navBar = dashBoardsection.querySelector(".nav-bar");
+    const navBarLis = navBar.querySelectorAll("li");
 
+    navBar.onclick = (e)=>{
+        e.preventDefault();
+
+        if (e.target.nodeName != "A")
+            return;
+        
+        const navAnchor = e.target;
+
+        let pointedNavBarLi = null;
+
+        for (let i = 0; i < navBarLis.length; i++) {
+            if (navBarLis[i].classList.contains("pointed")) {
+                pointedNavBarLi = navBarLis[i];
+                break;
+            }
+        }
+
+        if (pointedNavBarLi !== navAnchor.parentNode) {
+            navAnchor.parentNode.classList.add("pointed");
+            pointedNavBarLi.classList.remove("pointed");
+        }
+    };
 });
